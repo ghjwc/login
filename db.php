6e4fd291ade2c4b55dc90ab0c326684a6ea5b6ad
jwc <612346@naver.com>
2022-05-17 14:03:53 +0900

    php

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+    $user = 'root';
+    $db = 'hyosung3';
+    
+    $conn = mysqli_connect($host, $user);
+    // DB 서버 접속
+    $db_conn = mysqli_select_db($conn, $db);
+    // DB 선택
+
+
+    if($db_conn) {
+        echo 'DB 접속 성공!';
+    } else {
+        echo '실패';
+    }
+    ?>
